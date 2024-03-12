9d9f3fe4e22247b415bdaa9a8885e90804a357f3
AbdullohDev07 <gamesupro01@gmail.com>
2024-03-12 09:05:06 +0500

    my change one

---
+++ src/BookStore.Application/useCases/Books/Commands/DeleteBookCommand.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class DeleteBookCommand : IRequest<Book>
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ src/BookStore.Application/useCases/Books/Commands/DeleteBookCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.Application.Abstractions;
+using BookStore.Domain.Entities;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class DeleteBookCommandHandler
+    {
+        public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, Book>
+        {
+            private readonly IAppDbContext _context;
+            private readonly IMapper _mapper;
+            public DeleteBookCommandHandler(IAppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Book> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                _context.Books.Remove(user);
+
+                await _context.SaveChangesAsync();
+
+                return user;
+            }
+        }
+    }
+}
