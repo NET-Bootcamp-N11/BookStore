243a624bd51fa2cf83555ad8f7b1ac14878827d5
Tursunboev0227 <hayotillotursunboev7@gmail.com>
2024-03-12 08:56:59 +0500

    CreateBookCommandHAndler done

--- src/BookStore.Application/useCases/Books/Commands/CreateBookCommandHandler.cs
+++ src/BookStore.Application/useCases/Books/Commands/CreateBookCommandHandler.cs
@@ -1,13 +1,25 @@
-﻿using BookStore.Domain.Entities;
+﻿using BookStore.Application.Abstractions;
+using BookStore.Domain.Entities;
+using MapsterMapper;
 using MediatR;
 
 namespace BookStore.Application.useCases.Books.Commands
 {
     public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, Book>
     {
-        public Task<Book> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        private readonly IAppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+        public CreateBookCommandHandler(IAppDbContext appDbContext, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+        public async Task<Book> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = _mapper.Map<Book>(request);
+            var res = await _appDbContext.Books.AddAsync(book);
+            await _appDbContext.SaveChangesAsync();
+            return res.Entity;
         }
     }
 }
