06384e073291dc9826482f2bd187a94b36a4ba11
Abu_Programmiy <abdukholiq23@gmail.com>
2024-03-12 09:09:45 +0500

    O'z vohtining eeng zo'ri

---
+++ src/BookStore.Application/useCases/Authors/Queries/GetAllAuthorsHandler.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Application.Abstractions;
+using BookStore.Domain.Entities;
+using MediatR;
+
+namespace BookStore.Application.useCases.Authors.Queries
+{
+    public class GetAllAuthorsHandler : IRequestHandler<GetAllAuthorsQuery, IEnumerable<Author>>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetAllAuthorsHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task<IEnumerable<Author>> Handle(GetAllAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult((IEnumerable<Author>)_appDbContext.Author.ToList());// chetkiy
+        }
+    }
+}
---
+++ src/BookStore.Application/useCases/Authors/Queries/GetAllAuthorsQuery.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Authors.Queries
+{
+    public class GetAllAuthorsQuery:IRequest<IEnumerable<Author>>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
