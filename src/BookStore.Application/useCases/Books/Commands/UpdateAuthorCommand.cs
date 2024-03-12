d95f38bfeea931d580d072ccc636aae2193d9152
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-12 09:13:18 +0500

    UpdateAuthorCommand and Handler added

---
+++ src/BookStore.Application/useCases/Books/Commands/UpdateAuthorCommand.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class UpdateAuthorCommand: IRequest<Author>
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/BookStore.Application/useCases/Books/Commands/UpdateAuthorCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Application.Abstractions;
+using BookStore.Domain.Entities;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class UpdateAuthorCommandHandler: IRequestHandler<UpdateAuthorCommand, Author>
+    {
+
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+        public UpdateAuthorCommandHandler(IAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Author> Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var author = await _context.Author.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (author is not null)
+            {
+                author.Name = request.Name;
+                author.Description = request.Description;
+
+                await _context.SaveChangesAsync();
+            }
+
+            return null;
+        }
+
+    }
+}
