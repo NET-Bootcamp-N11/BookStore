5719666937ab0e02cc0d0c3594d76d7b3c27950c
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-03-12 09:59:07 +0500

    Complete GetGenreByIdQuery and handler

---
+++ src/BookStore.Application/useCases/Genres/Queries/GetGenreByIdQuery.cs
@@ -0,0 +1,10 @@
+using BookStore.Domain.Entities;
+using MediatR;
+
+namespace BookStore.Application.useCases.Genres.Queries
+{
+    public class GetGenreByIdQuery : IRequest<Genre>
+    {
+        public long Id { get; set; }
+    }
+}
