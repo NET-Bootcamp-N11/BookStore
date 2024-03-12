a3eb9b966d8d7f0be746634417df49adbaa9192f
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-12 08:37:44 +0500

    Application DI implemented

---
+++ src/BookStore.Application/useCases/Books/Commands/CreateBookCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Application.useCases.Books.Commands
+{
+    public class CreateBookCommand
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ src/BookStore.Application/useCases/Books/Queries/GetBookByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Domain.Entities;
+
+namespace BookStore.Application.useCases.Books.Queries
+{
+    public class GetBookByIdQuery
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Author Author { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
