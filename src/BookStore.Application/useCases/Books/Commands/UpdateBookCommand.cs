a637b075081b477b516b692038eb202ca408e45c
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-12 09:08:26 +0500

    add update function

---
+++ src/BookStore.Application/useCases/Books/Commands/UpdateBookCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class UpdateBookCommand:IRequest
+
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ src/BookStore.Application/useCases/Books/Commands/UpdateBookCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Application.Abstractions;
+using BookStore.Domain.Entities;
+using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Books.Commands
+{
+    public class UpdateBookCommandHandler:IRequest<UpdateBookCommand>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+
+        public UpdateBookCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<Book> Handle(UpdateBookCommand updateBookCommand, CancellationToken cancellationToken)
+        {
+            var book = await _appDbContext.Books.FirstOrDefaultAsync(x => x.Id == updateBookCommand.Id);
+            if (book != null)
+            {
+                book = updateBookCommand.Adapt<Book>();
+                _appDbContext.SaveChangesAsync();
+                return book;
+            }
+            return null;
+        }
+
+    }
+}
