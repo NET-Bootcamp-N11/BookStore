1dc42120d2148482696b9f019a7dfa5b3f8ea5b1
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-12 09:12:30 +0500

    add update

--- src/BookStore.Application/useCases/Books/Commands/UpdateBookCommand.cs
+++ src/BookStore.Application/useCases/Books/Commands/UpdateBookCommand.cs
@@ -1,13 +1,8 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookStore.Application.useCases.Books.Commands
 {
-    public class UpdateBookCommand:IRequest
+    public class UpdateBookCommand : IRequest
 
     {
         public long Id { get; set; }
