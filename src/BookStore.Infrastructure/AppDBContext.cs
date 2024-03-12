cea861b220570cb7bb95bb250238901fcc353e84
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-12 10:57:57 +0500

    Authors dbset changed to Authors

--- src/BookStore.Infrastructure/AppDBContext.cs
+++ src/BookStore.Infrastructure/AppDBContext.cs
@@ -7,8 +7,8 @@ namespace BookStore.Infrastructure
     public class AppDBContext : DbContext, IAppDbContext
     {
         public DbSet<Book> Books { get; set; }
-        public DbSet<Author> Author { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         async ValueTask<int> IAppDbContext.SaveChangesAsync(CancellationToken cancellationToken)
             => await base.SaveChangesAsync(cancellationToken);
