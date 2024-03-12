c2eae1ca3769fd8e45001d4f8fc5905f84625cd2
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-12 08:33:25 +0500

    Auhtor added

---
+++ src/BookStore.Application/useCases/Authors/Commands/CreateAuthorCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Authors.Commands
+{
+    public class CreateAuthorCommand : IRequest<string>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/BookStore.Application/useCases/Authors/Commands/CreateAuthorCommandHanler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Authors.Commands
+{
+    public class CreateAuthorCommandHanler : IRequestHandler<CreateAuthorCommand, string>
+    {
+        public Task<string> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
