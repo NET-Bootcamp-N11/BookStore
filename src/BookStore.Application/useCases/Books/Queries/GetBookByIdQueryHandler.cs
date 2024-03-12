0fe7597683734d5a8ebed653e66e7845441de32b
Mr. Otabek <146438558+OtabekgithubJava@users.noreply.github.com>
2024-03-12 21:08:48 +0500

    Update GetBookByIdQueryHandler.cs
    Update

--- src/BookStore.Application/useCases/Books/Queries/GetBookByIdQueryHandler.cs
+++ src/BookStore.Application/useCases/Books/Queries/GetBookByIdQueryHandler.cs
@@ -1,6 +1,18 @@
 ﻿namespace BookStore.Application.useCases.Books.Queries
 {
-    public class GetBookByIdQueryHandler
+    public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, Book>
     {
+        private readonly IAppDbContext _appDb;
+
+        public GetBookByIdQueryHandler(IAppDbContext appDb)
+        {
+            _appDb = appDb;
+        }
+
+        public async Task<Book> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        {
+            var book = await _appDb.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return book;
+        }
     }
 }
