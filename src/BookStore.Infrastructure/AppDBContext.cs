a74c8777f9060913a22e49d7254f17db1e4a48e6
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-03-15 00:47:38 +0500

    Seed Data successfully

--- src/BookStore.Infrastructure/AppDBContext.cs
+++ src/BookStore.Infrastructure/AppDBContext.cs
@@ -6,9 +6,6 @@ namespace BookStore.Infrastructure
 {
     public class AppDBContext : DbContext, IAppDbContext
     {
-        //public AppDBContext(DbContextOptions<AppDBContext> options)
-        //    : base(options) => Database.Migrate();
-
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Author> Authors { get; set; }
@@ -22,6 +19,59 @@ namespace BookStore.Infrastructure
             optionsBuilder
                 .UseLazyLoadingProxies()
                 .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookStore.MVC.DB;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genre>()
+                .HasData(
+                    new List<Genre>
+                    {
+                        new Genre
+                        {
+                            Id = 1,
+                            Name = "Tarixiy"
+                        },
+                        new Genre
+                        {
+                            Id = 2,
+                            Name = "Romantic"
+                        },
+                        new Genre
+                        {
+                            Id = 3,
+                            Name = "Melodrama"
+                        },
+                        new Genre
+                        {
+                            Id = 4,
+                            Name = "Fantastic"
+                        }
+                    }
+                );
+            modelBuilder.Entity<Author>()
+                .HasData(
+                    new List<Author>
+                    {
+                        new Author{ Id = 1, Name = "O'tkir Hoshimov", Description = "Zo'r inson"},
+                        new Author{ Id = 2, Name = "Abdulla Qodiriy", Description = "Zo'r inson"},
+                        new Author{ Id = 3, Name = "Xudoyberdi To'xtaboyev", Description = "Zo'r inson"},
+                        new Author{ Id = 4, Name = "Asqad Maxtor", Description = "Zo'r inson"},
+
+                    }
+                );
+            modelBuilder.Entity<Book>()
+                .HasData(
+                    new List<Book>
+                    {
+                        new Book{ Id = 1, AuthorId = 1, Title = "Dunyoning ishlari", Description = "Zo'r kitob"},
+                        new Book{ Id = 2, AuthorId = 2, Title = "O'tgan kunlar", Description = "Zo'r kitob"},
+                        new Book{ Id = 3, AuthorId = 3, Title = "Besh bolali yigitcha", Description = "Zo'r kitob"},
+                        new Book{ Id = 4, AuthorId = 4, Title = "Chinor", Description = "Zo'r kitob"},
+                    }
+                );
         }
     }
 }
