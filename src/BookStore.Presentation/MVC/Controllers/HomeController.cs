48ef0f323051b533ae0543f99ee699fa557c32e5
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-16 21:12:28 +0500

    new

--- src/BookStore.Presentation/MVC/Controllers/HomeController.cs
+++ src/BookStore.Presentation/MVC/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BookStore.Application.useCases.Authors.Queries;
 using BookStore.Application.useCases.Books.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,12 @@ namespace MVC.Controllers
             var getAllBooks = new GetAllBooksQuery();
             var books = await _mediator.Send(getAllBooks);
             return View(books);
+        }
+        public async Task<IActionResult> AuthorsController() 
+        {
+            var getAllAuthors = new GetAllAuthorsQuery();
+            var Authors = await _mediator.Send(getAllAuthors);
+            return View(Authors);
         }
 
         public IActionResult Privacy()
