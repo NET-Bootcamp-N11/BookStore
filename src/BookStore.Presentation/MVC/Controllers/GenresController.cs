89b088955d942333a4010a71a8bf10838e83b96d
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-16 05:24:28 +0500

    add `GetAllGenres` #42

--- src/BookStore.Presentation/MVC/Controllers/GenresController.cs
+++ src/BookStore.Presentation/MVC/Controllers/GenresController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Application.useCases.Genres.Queries;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
 
 namespace MVC.Controllers
 {
     public class GenresController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public GenresController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            GetAllGenreQuery? query = new GetAllGenreQuery();
+            var genres = await _mediator.Send(query);
+            return View(genres);
         }
     }
 }
