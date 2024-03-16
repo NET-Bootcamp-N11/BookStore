048f8c531af3b562c2f766c1bb8e994604ae85ef
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-16 05:42:12 +0500

    Add `DeleteGenreById`  ##IssueNotFound

---
+++ src/BookStore.Application/useCases/Genres/Commands/DeleteGenreByIdCommand.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.useCases.Genres.Commands
+{
+    public class DeleteGenreByIdCommand :IRequest<Genre>
+    {
+        public int Id { get; set; }
+    }
+}
