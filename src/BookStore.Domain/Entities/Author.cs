03b4df7634e4a57c4a3f1c5e8a01cc616253da39
Sardor <sardorstudent0618@gmail.com>
2024-03-17 15:41:02 +0500

    Entities edited

--- src/BookStore.Domain/Entities/Author.cs
+++ src/BookStore.Domain/Entities/Author.cs
@@ -1,9 +1,11 @@
 ﻿namespace BookStore.Domain.Entities
 {
-    public class Author 
+    public class Author
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public virtual List<Book> Books { get; set; }
     }
 }
