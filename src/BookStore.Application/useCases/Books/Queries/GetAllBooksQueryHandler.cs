3ec809ca4d271dbe40a6ce114e5e94b74171d4a9
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-18 15:32:47 +0500

    new

--- src/BookStore.Application/useCases/Books/Queries/GetAllBooksQueryHandler.cs
+++ src/BookStore.Application/useCases/Books/Queries/GetAllBooksQueryHandler.cs
@@ -1,22 +1,33 @@
 ﻿using BookStore.Application.Abstractions;
+using BookStore.Application.useCases.Books.Queries;
 using BookStore.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
-namespace BookStore.Application.useCases.Books.Queries
+namespace BookStore.Application.UseCases.Books.Queries
 {
     public class GetAllBooksQueryHandler : IRequestHandler<GetAllBooksQuery, List<Book>>
     {
         private readonly IAppDbContext _appDbContext;
+        private readonly IMemoryCache _memoryCache;
 
-        public GetAllBooksQueryHandler(IAppDbContext context)
+        public GetAllBooksQueryHandler(IAppDbContext context, IMemoryCache memoryCache)
         {
             _appDbContext = context;
+            _memoryCache = memoryCache;
         }
 
         public async Task<List<Book>> Handle(GetAllBooksQuery request, CancellationToken cancellationToken)
         {
-            return await _appDbContext.Books.ToListAsync(cancellationToken);
+
+            if (!_memoryCache.TryGetValue("Books", out List<Book> books))
+            {
+                books = await _appDbContext.Books.ToListAsync(cancellationToken);
+                _memoryCache.Set("Books", books);
+            }
+
+            return books;
         }
     }
 }
