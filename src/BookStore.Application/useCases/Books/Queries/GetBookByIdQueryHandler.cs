9dc91deafc13db7718669bfe6ac025b7805be829
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-18 15:34:59 +0500

    add memory cache to book

--- src/BookStore.Application/useCases/Books/Queries/GetBookByIdQueryHandler.cs
+++ src/BookStore.Application/useCases/Books/Queries/GetBookByIdQueryHandler.cs
@@ -1,23 +1,41 @@
 using BookStore.Application.Abstractions;
+using BookStore.Application.useCases.Books.Queries;
 using BookStore.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace BookStore.Application.useCases.Books.Queries
+namespace BookStore.Application.UseCases.Books.Queries
 {
     public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, Book>
     {
         private readonly IAppDbContext _appDb;
+        private readonly IMemoryCache _memoryCache;
 
-        public GetBookByIdQueryHandler(IAppDbContext appDb)
+        public GetBookByIdQueryHandler(IAppDbContext appDb, IMemoryCache memoryCache)
         {
             _appDb = appDb;
+            _memoryCache = memoryCache;
         }
 
         public async Task<Book> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
         {
-            var user = await _appDb.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
-            return user;
+           
+            if (!_memoryCache.TryGetValue($"Book_{request.Id}", out Book book))
+            {
+              
+                book = await _appDb.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+               
+                if (book != null)
+                {
+                    _memoryCache.Set($"Book_{request.Id}", book);
+                }
+            }
+
+            return book;
         }
     }
 }
