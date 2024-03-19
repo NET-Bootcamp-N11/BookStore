7afc61d33551d7985765731f8eb147357d08ad19
Sardor <sardorstudent0618@gmail.com>
2024-03-19 15:51:25 +0500

    Book Update Action Fixed

--- src/BookStore.Application/useCases/Books/Commands/UpdateBookCommandHandler.cs
+++ src/BookStore.Application/useCases/Books/Commands/UpdateBookCommandHandler.cs
@@ -15,22 +15,21 @@ namespace BookStore.Application.useCases.Books.Commands
         public async Task<Book> Handle(UpdateBookCommand updateBookCommand, CancellationToken cancellationToken)
         {
             var book = await _appDbContext.Books.FirstOrDefaultAsync(x => x.Id == updateBookCommand.Id);
-            book.Genres = new List<Genre>();
-            await _appDbContext.SaveChangesAsync();
-
-            var genres = await _appDbContext.Genres.Where(x => updateBookCommand.Genres.Contains(x.Id)).ToListAsync();
-
-            //var genres = await _appDbContext.Genres.Where(x => updateBookCommand.Genres.Contains(x.Id) && !book.Genres.Select(x => x.Id).Contains(x.Id)).ToListAsync();
 
             if (book == null)
-            {
                 throw new Exception("Book not found");
-            }
+
+            var genres = _appDbContext.Genres.Where(x => updateBookCommand.Genres.Contains(x.Id)).ToList();
 
             book.Title = updateBookCommand.Title;
             book.Description = updateBookCommand.Description;
             book.AuthorId = updateBookCommand.AuthorId;
-            book.Genres = genres;
+
+            book.Genres.Clear();  // Clear existing genres
+            foreach (var genre in genres)
+            {
+                book.Genres.Add(genre);  // Add selected genres
+            }
 
             var entry = _appDbContext.Books.Update(book);
             await _appDbContext.SaveChangesAsync();
---
+++ src/BookStore.Presentation/MVC/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
