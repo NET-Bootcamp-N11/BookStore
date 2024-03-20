f919d0bacda9731e72f0abd69d23c0fc6cf1fcb8
Tursunboev0227 <hayotillotursunboev7@gmail.com>
2024-03-20 08:18:03 +0500

    author search done

---
+++ src/BookStore.Presentation/MVC/Models/BookSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Domain.Entities;
+
+namespace MVC.Models
+{
+    public class BookSearchViewModel
+    {
+        public List<Author> Authors { get; set; }
+        public List<Author> SearchedAuthors { get; set; }
+    }
+}
