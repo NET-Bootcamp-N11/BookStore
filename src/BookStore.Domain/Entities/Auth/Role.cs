a81431b2379da6ec9cc711ef31edfa67f03bc5a4
Sardor <sardorstudent0618@gmail.com>
2024-03-22 17:59:39 +0500

    Auth implemented with Asp.Net.Identity

---
+++ src/BookStore.Domain/Entities/Auth/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.Domain.Entities.Auth
+{
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
---
+++ src/BookStore.Presentation/MVC/Models/Auth/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC.Models.Auth
+{
+    public class LoginDTO
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
