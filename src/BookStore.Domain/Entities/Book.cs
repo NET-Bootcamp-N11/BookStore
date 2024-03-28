e9052575111468f14284bd2719e16d57e2cc1013
Sardor <sardorstudent0618@gmail.com>
2024-03-28 12:59:33 +0500

    Book Entity Updated

--- src/BookStore.Domain/Entities/Book.cs
+++ src/BookStore.Domain/Entities/Book.cs
@@ -8,6 +8,8 @@ namespace BookStore.Domain.Entities
         public string Title { get; set; }
         [MinLength(20)]
         public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PDFFilePath { get; set; }
         public int AuthorId { get; set; }
 
         public virtual Author Author { get; set; }
