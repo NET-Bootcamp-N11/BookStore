ca691ea2f41eb906a25110d3bfd1195c901c4878
Sardor <sardorstudent0618@gmail.com>
2024-04-02 23:02:46 +0500

    Configurations Updated

---
+++ src/BookStore.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Domain.Entities.Auth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(new User()
+            {
+                Id = Guid.Parse("ca9d3855-2c7d-427a-9364-d37dac608b55"),
+                FullName = "Admin aka",
+                UserName = "adminaka",
+                NormalizedUserName = "ADMINAKA",
+                Email = "adminaka0618@gmail.com",
+                NormalizedEmail = "ADMINAKA0618@GMAIL.COM",
+                PasswordHash = "AQAAAAIAAYagAAAAEL5dGfxjT0/cQfyvMMMQ+b+ancTXrKrIV/xliyQbGTpwoIO2zJNi/DYKFHKMs05POg==",
+                SecurityStamp = "EQL6PMQHTWTUEC7XXDY6ZS5M3YS6UAZJ",
+                ConcurrencyStamp = "fa95c310-1ae7-454d-a589-af70ed8c0bce",
+                PhoneNumber = "123456789"
+            });
+        }
+    }
+}
---
+++ src/BookStore.Infrastructure/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.Infrastructure.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+        {
+            builder.HasData(new IdentityUserRole<Guid>
+            {
+                RoleId = Guid.Parse("814a9fe9-4f17-4fb0-a10f-0cdda6d837c1"),
+                UserId = Guid.Parse("ca9d3855-2c7d-427a-9364-d37dac608b55")
+            });
+        }
+    }
+}
