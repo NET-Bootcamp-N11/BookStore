7c3717ddb71ef88760ec9830576f2d40e8db8627
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-29 12:17:49 +0500

    INFRA: Initalize Build Project

---
+++ src/BookStore.Infrastructure.Build/Program.cs
@@ -0,0 +1,84 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipline = new GithubPipeline
+{
+    Name = "BookStore Build Pipline",
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+    },
+
+    Jobs = new Dictionary<string, Job>
+      {
+          {
+              "build",
+              new Job
+              {
+                  RunsOn = BuildMachines.Windows2022,
+
+                  Steps = new List<GithubTask>
+                  {
+                      new CheckoutTaskV2
+                      {
+                          Name = "Check out"
+                      },
+
+                      new SetupDotNetTaskV1
+                      {
+                          Name = "Setup .Net",
+
+                          TargetDotNetVersion = new TargetDotNetVersion
+                          {
+                              DotNetVersion = "8.0.204",
+                              IncludePrerelease = true
+                          }
+                      },
+
+                      new RestoreTask
+                      {
+                          Name = "Restore"
+                      },
+
+                      new DotNetBuildTask
+                      {
+                          Name = "Build"
+                      },
+
+                      new TestTask
+                      {
+                          Name = "Test"
+                      }
+                  }
+              }
+          }
+      }
+};
+
+var client = new ADotNetClient();
+
+
+string buildScriptPath = "../../../../../.github/workflows/dotnet.yml";
+string directoryPath = Path.GetDirectoryName(buildScriptPath)!;
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+client.SerializeAndWriteToFile
+(
+    adoPipeline: githubPipline,
+    path: buildScriptPath
+);
